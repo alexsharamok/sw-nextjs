4731bb3ac657cc4beb6a42ad1cf74034c999353b
alex sharamok <alexanders@uniform.dev>
2024-04-08 15:55:06 +0300

    added fallback blocking

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -30,7 +30,7 @@ export async function getStaticPaths() {
     // specify all static paths and let nextjs handle 404
     return {
       paths: await getStaticPaths(),
-      fallback: false,
+      fallback: "blocking",
     };
   } else {
     // If we are running the site in dynamic preview then
