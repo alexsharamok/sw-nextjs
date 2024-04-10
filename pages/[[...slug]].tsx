b704cf7decee0090f288e4c7eb8741c2092367d3
alex sharamok <alexanders@uniform.dev>
2024-04-10 16:29:32 +0300

    isr testing

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createConsoleLogger, getNextConfig, getNextPageProps, NextPageProps, PageComponent, Placeholder, UniformContextProvider } from "@uniformdev/next";
-import { getPageProps } from "@uniformdev/common-client";
+import { downloadSiteMap, parseUniformServerConfig } from "@uniformdev/common-server";
+import { notFound } from 'next/navigation';
 
 
 // Register React components here if you migrate from MVC to React
@@ -24,17 +25,20 @@ export async function getStaticPaths() {
   // await import is essential here because getStaticPaths only server-side 
   // and import must only happen server side (on client-side the code will blow up)
   const { getStaticPaths } = await import("@uniformdev/next-server");
+  
 
   if (process.env.UNIFORM_BUILD_MODE !== 'ssr') {
     // If we are exporting the site directly or via a publish
     // specify all static paths and let nextjs handle 404
+    const paths = await getStaticPaths();
+    
     return {
-      paths: await getStaticPaths(),
+      paths: paths,
       fallback: "blocking",
     };
   } else {
     // If we are running the site in dynamic preview then
-    // handle all paths and render them on the fly
+    // handle all paths and render them on the fly    
     return {
       paths: [],
       fallback: "blocking",
@@ -48,6 +52,28 @@ export async function getStaticProps({ params }) {
   const itemId = await tryFindItemId(asPath);
   const timestamp = await tryGetTimestamp(asPath);
   const props = await getNextPageProps({ asPath, itemId, timestamp });
+  
+  const logger = createConsoleLogger();
+  let map: any = undefined;    
+    const config = parseUniformServerConfig(process.env, logger);    
+    try {      
+      map = await downloadSiteMap(
+          config,
+          logger,
+          {
+              maxAttempts: 1,
+          }
+      );
+  } catch {
+      logger.warn('cannot download map');
+  }
+  if (map) {
+    if (!Object.getOwnPropertyNames(map).includes(asPath)) {
+      return {};
+    } 
+  }
+  console.log(asPath);
+
   return { 
     props: props,
     revalidate: 60
